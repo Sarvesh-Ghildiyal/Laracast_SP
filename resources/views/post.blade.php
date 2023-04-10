fb8cb1dc5386f01362c6141eacd27ae2a4309161
Sarvesh-Ghildiyal <sarveshghildiyal@gmail.com>
2023-04-11 01:57:36 +0530

    initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+<title>My Blog</title>
+<link rel="stylesheet" href="{{Url('posts.css')}}">   <!--href="/posts.css"-->
+<article>
+    
+    {{-- variabl neded to et dynamic page --}}
+    <?= $post; ?>
+         <br>
+    <a href="/">Go Back</a>
+</article>
+
+
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,29 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>BlogPost</title>
+        <link rel="stylesheet" href="{{Url('posts.css')}}">
+    </head>
+    <body>
+        <article>
+            <h1><a href="/posts/my-first-post">My First Post</a></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore expedita fugit adipisci totam ullam nam dolor! Provident omnis, reprehenderit, aliquam id, sapiente modi deleniti possimus vero officia repellendus sed exercitationem ducimus delectus laborum ratione numquam iste iure facere voluptatem! Deserunt ratione distinctio assumenda! Cupiditate ab corrupti excepturi adipisci aliquam ullam est iure sed ipsa, beatae recusandae totam repellendus asperiores accusamus ipsam! Temporibus adipisci similique eos consequuntur aliquam itaque minus eveniet voluptatum soluta distinctio sequi eligendi ratione dignissimos incidunt quas impedit explicabo, facere animi! Qui adipisci repellat laudantium saepe placeat consequatur tempore ad! Amet consequatur
+                 facilis iusto, esse ipsam exercitationem laboriosam?</p>
+        </article>
+
+        <article>
+            <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore expedita fugit adipisci totam ullam nam dolor! Provident omnis, reprehenderit, aliquam id, sapiente modi deleniti possimus vero officia repellendus sed exercitationem ducimus delectus laborum ratione numquam iste iure facere voluptatem! Deserunt ratione distinctio assumenda! Cupiditate ab corrupti excepturi adipisci aliquam ullam est iure sed ipsa, beatae recusandae totam repellendus asperiores accusamus ipsam! Temporibus adipisci similique eos consequuntur aliquam itaque minus eveniet voluptatum soluta distinctio sequi eligendi ratione dignissimos incidunt quas impedit explicabo, facere animi! Qui adipisci repellat laudantium saepe placeat consequatur tempore ad! Amet consequatur
+                facilis iusto, esse ipsam exercitationem laboriosam?</p>
+        </article>
+
+        <article>
+            <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore expedita fugit adipisci totam ullam nam dolor! Provident omnis, reprehenderit, aliquam id, sapiente modi deleniti possimus vero officia repellendus sed exercitationem ducimus delectus laborum ratione numquam iste iure facere voluptatem! Deserunt ratione distinctio assumenda! Cupiditate ab corrupti excepturi adipisci aliquam ullam est iure sed ipsa, beatae recusandae totam repellendus asperiores accusamus ipsam! Temporibus adipisci similique eos consequuntur aliquam itaque minus eveniet voluptatum soluta distinctio sequi eligendi ratione dignissimos incidunt quas impedit explicabo, facere animi! Qui adipisci repellat laudantium saepe placeat consequatur tempore ad! Amet consequatur
+                facilis iusto, esse ipsam exercitationem laboriosam?</p>
+        </article>
+    </body>
+    </html>
